d2d0fc9e793cf79f838917485739453d0f18e21c
Itszuvalex <Itszuvalex@gmail.com>
2015-08-27 01:38:01 -0400

    Crystal Mount, Power Pedestal.  TilePowerPedestal implementation basic.

--- src/main/scala/com/itszuvalex/femtocraft/core/Power/IPowerPedestal.scala
+++ src/main/scala/com/itszuvalex/femtocraft/core/Power/IPowerPedestal.scala
@@ -1,5 +1,7 @@
 package com.itszuvalex.femtocraft.core.Power
 
+import com.itszuvalex.itszulib.api.core.Loc4
+
 /**
  * Created by Christopher Harris (Itszuvalex) on 8/26/15.
  */
@@ -33,4 +35,23 @@ trait IPowerPedestal {
    */
   def consume(amount: Long, doConsume: Boolean): Long
 
+
+  /**
+   *
+   * @return Direction of the mount this is connected to.  UNKNOKWN otherwise.
+   */
+  def mountLoc: Loc4
+
+  /**
+   *
+   * @param loc Location to accept mount connection at.
+   * @return True if mount can be added to this location.
+   */
+  def canSetMount(loc: Loc4): Boolean
+
+  /**
+   *
+   * @param loc Location of mount.
+   */
+  def setMount(loc: Loc4): Unit
 }
