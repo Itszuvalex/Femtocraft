d5245e970c7925fc0510eaa9bf91c95182c878b3
Itszuvalex <Itszuvalex@gmail.com>
2015-09-20 02:26:15 -0400

    Render box for controller for arc furnace increasded

--- src/main/scala/com/itszuvalex/femtocraft/industry/tile/TileArcFurnace.scala
+++ src/main/scala/com/itszuvalex/femtocraft/industry/tile/TileArcFurnace.scala
@@ -6,6 +6,7 @@ import com.itszuvalex.femtocraft.{Femtocraft, GuiIDs}
 import com.itszuvalex.itszulib.api.core.Configurable
 import com.itszuvalex.itszulib.core.TileEntityBase
 import com.itszuvalex.itszulib.core.traits.tile.{MultiBlockComponent, TileFluidTank}
+import net.minecraft.util.AxisAlignedBB
 import net.minecraftforge.common.util.ForgeDirection
 import net.minecraftforge.fluids.{Fluid, FluidTank, IFluidTank}
 
@@ -38,4 +39,11 @@ import net.minecraftforge.fluids.{Fluid, FluidTank, IFluidTank}
    * @return The type of PowerNode this is.
    */
   override def getType: String = IPowerNode.DIFFUSION_TARGET_NODE
+
+  override def getRenderBoundingBox: AxisAlignedBB = {
+    if (isController) {
+      AxisAlignedBB.getBoundingBox(xCoord, yCoord, zCoord, xCoord + 2, yCoord + 3, zCoord + 2)
+    }
+    else super.getRenderBoundingBox
+  }
 }
